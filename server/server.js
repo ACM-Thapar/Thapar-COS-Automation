9252904f7b450f815ef409acee80aa477ca56ebb
Esha-Sherring <esherring2016@gmail.com>
2020-09-16 11:09:13 -0700

    auth routes done

--- server/server.js
+++ server/server.js
@@ -1,15 +1,18 @@
 const express = require('express');
+const cors = require('cors');
 const connectdb = require('./config/db');
 const app = express();
+const authroutes = require('./routes/authroutes.js');
 
 const PORT = process.env.PORT || 5000;
+app.use(cors());
 connectdb();
 const server = app.listen(PORT, console.log(`Server started on Port ${PORT}`));
 
 //add middlewares
-
+app.use(express.json({ extended: false }));
 //add routes
-
+app.use('/api/auth', authroutes);
 // Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
   console.log(`Error: ${err.message}`);
