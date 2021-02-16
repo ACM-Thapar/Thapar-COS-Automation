81261d77092d38d55ddac06972b03c8fedaa7ccf
aniketbiswas21 <aniket.biswas75@gmail.com>
2021-02-16 12:56:07 +0530

    made the req changes

--- server/controllers/usercontrollers.js
+++ server/controllers/usercontrollers.js
@@ -10,6 +10,7 @@ const nodemailer = require('nodemailer');
 
 // *Models
 const User = require('../models/user');
+const Shopkeeper = require('../models/shopkeeper');
 
 // @desc     Register User
 // @route    POST /api/user/signup
@@ -20,16 +21,25 @@ module.exports.post_signup = async (req, res) => {
     return res.status(400).json({ errors: errors.array() });
   }
 
-  const { name, phone, email, password, hostel } = req.body;
+  const { email } = req.body;
 
   try {
     // Domain check for thapar.edu
     if (!email.includes('@thapar.edu')) {
       return res.status(400).json('Enter a thapar edu email');
     }
-    let user = await User.findOne({ email: email });
+
+    let [user, shopkeeper] = await Promise.all([
+      User.findOne({ email: email }).lean().exec(),
+      Shopkeeper.findOne({ email: email }).lean().exec(),
+    ]);
     if (user) {
       return res.status(400).json('user already exists');
+    }
+    if (shopkeeper) {
+      return res
+        .status(400)
+        .json('shopkeeper with the same email already exists');
     }
 
     //Generating an otp
@@ -38,16 +48,14 @@ module.exports.post_signup = async (req, res) => {
       specialChars: false,
     });
     console.log(otp);
-    const newValue = {
-      name,
-      phone,
-      email,
-      password,
-      hostel,
-      otp: { code: otp },
+    const newBody = {
+      ...req.body,
+      otp: {
+        code: otp,
+      },
     };
 
-    user = await User.create(newValue);
+    user = await User.create(newBody);
     const message = `Thanks for registering! We will need to verify your email first. You can do so by entering ${user.otp.code}. This code is valid for only next 15 minutes.`;
 
     await sendEmail({
@@ -55,6 +63,52 @@ module.exports.post_signup = async (req, res) => {
       subject: 'Verification OTP',
       message,
     });
+
+    sendTokenResponse(user, 200, req, res);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send('server error');
+  }
+};
+
+// @desc     Register Firebase User
+// @route    POST /api/user/firebase-signup
+// @access   Public
+module.exports.firebaseRegister = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { email } = req.body;
+
+  try {
+    // Domain check for thapar.edu
+    if (!email.includes('@thapar.edu')) {
+      return res.status(400).json('Enter a thapar edu email');
+    }
+
+    let [user, shopkeeper] = await Promise.all([
+      User.findOne({ email: email }).lean().exec(),
+      Shopkeeper.findOne({ email: email }).lean().exec(),
+    ]);
+    if (user) {
+      return res.status(400).json('user already exists');
+    }
+    if (shopkeeper) {
+      return res
+        .status(400)
+        .json('shopkeeper with the same email already exists');
+    }
+
+    const newBody = {
+      ...req.body,
+      isGoogleUser: true,
+      isPhoneVerified: true,
+      verified: true,
+    };
+
+    user = await User.create(newBody);
 
     sendTokenResponse(user, 200, req, res);
   } catch (err) {
@@ -196,10 +250,50 @@ module.exports.getMe = async (req, res) => {
   }
 };
 
+// @desc     Edit/Complete Profile
+// @route    PUT /api/user/complete-profile
+// @access   Private
+
+module.exports.completeProfile = async (req, res) => {
+  try {
+    if (req.body.email) {
+      return res.status(400).json({
+        success: false,
+        data: 'Email cannot be updated',
+      });
+    }
+    const updateData = { ...req.body };
+    const user = await User.findByIdAndUpdate(req.user.id, updateData, {
+      new: true,
+      runValidators: true,
+    }).exec();
+
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        data: 'No user found',
+      });
+    }
+    const status = user.isCompleted;
+
+    res.status(200).json({
+      success: true,
+      data: user,
+      profileCompletion: status,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ success: false, data: err });
+  }
+};
+
 // Get token from model, create cookie and send response
 const sendTokenResponse = (user, statusCode, req, res) => {
   // Create token
   const token = user.getSignedJwtToken();
   req.session.token = token;
-  res.status(statusCode).json({ success: true, token });
+  const status = user.isCompleted;
+  res
+    .status(statusCode)
+    .json({ success: true, token, profileCompletion: status });
 };
